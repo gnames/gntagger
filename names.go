c16d3ce38e80592eaa7d40f72723957931b455ff
Alexander Myltsev <alexander@myltsev.com>
2017-11-07 21:53:06 +0300

    Remove obsolete func

--- names.go
+++ names.go
@@ -41,31 +41,6 @@ type Names struct {
 	Data gnfinder.OutputJSON
 }
 
-func (n *Names) Strings() []string {
-	namesTotal := len(n.Data.Names)
-	namesSliceWindow := 5
-	namesSliceLeft := names.Data.Meta.CurrentName - namesSliceWindow
-	if namesSliceLeft < 0 {
-		namesSliceLeft = 0
-	}
-	namesSliceRight := names.Data.Meta.CurrentName + namesSliceWindow
-	if namesSliceRight > namesTotal {
-		namesSliceRight = namesTotal
-	}
-	log.Printf("%d %d", namesSliceLeft, namesSliceRight)
-
-	//namesSlice := n.Data.Names[namesSliceLeft:namesSliceRight]
-	str := make([]string, (namesSliceRight - namesSliceLeft)*4)
-	for i := namesSliceLeft; i < namesSliceRight; i++ {
-		current := i == n.Data.Meta.CurrentName
-		v := names.Data.Names[i]
-		for j, w := range nameStrings(&v, current, i, namesTotal) {
-			str[(i-namesSliceLeft)*4+j] = w
-		}
-	}
-	return str
-}
-
 func (n *Names) Save() error {
 	json := n.Data.ToJSON()
 	return ioutil.WriteFile(n.Path, json, 0644)
