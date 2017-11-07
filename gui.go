8f24566404ecaa61757ce49256f25e639659ba4f
Alexander Myltsev <alexander@myltsev.com>
2017-11-07 18:44:13 +0300

    Remove redundant params

--- gui.go
+++ gui.go
@@ -91,19 +91,17 @@ func Keybindings(g *gocui.Gui) error {
 }
 
 func Layout(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-
-	err := viewNames(g, maxX, maxY)
+	err := viewNames(g)
 	if err != nil {
 		return err
 	}
 
-	err = viewText(g, maxX, maxY)
+	err = viewText(g)
 	if err != nil {
 		return err
 	}
 
-	err = viewHelp(g, maxX, maxY)
+	err = viewHelp(g)
 	if err != nil {
 		return err
 	}
@@ -111,7 +109,8 @@ func Layout(g *gocui.Gui) error {
 	return nil
 }
 
-func viewNames(g *gocui.Gui, maxX, maxY int) error {
+func viewNames(g *gocui.Gui) error {
+	_, maxY := g.Size()
 	if v, err := g.SetView("names", -1, 3, 35, maxY-1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
@@ -134,7 +133,8 @@ func viewNames(g *gocui.Gui, maxX, maxY int) error {
 	return nil
 }
 
-func viewText(g *gocui.Gui, maxX, maxY int) error {
+func viewText(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
 	if v, err := g.SetView("text", 35, 3, maxX, maxY-1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
@@ -145,7 +145,8 @@ func viewText(g *gocui.Gui, maxX, maxY int) error {
 	return nil
 }
 
-func viewHelp(g *gocui.Gui, maxX, maxY int) error {
+func viewHelp(g *gocui.Gui) error {
+	maxX, maxY := g.Size()
 	if v, err := g.SetView("help", -1, maxY-2, maxX, maxY); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
