6164e9de73423fb9db4b7f54c91427103b6b9a67
Alexander Myltsev <alexander@myltsev.com>
2017-11-16 18:11:28 +0300

    Fix #38: fit text to width

--- gui.go
+++ gui.go
@@ -11,7 +11,31 @@ import (
 	"os"
 )
 
+type Window struct {
+	x0, y0, x1, y1 int
+}
+
+func (w *Window) width() int {
+	return w.x1 - w.x0
+}
+
+func (w *Window) height() int {
+	return w.y1 - w.y0
+}
+
+type ViewType int
+
+const (
+	ViewText  ViewType = iota
+	ViewNames
+	ViewHelp
+)
+
 var (
+	textData     []byte
+	textDataPath string
+
+	views 				  = map[ViewType]*Window{}
 	names                 = &Names{}
 	text                  = &Text{}
 	saveCount             = 0
@@ -19,18 +43,18 @@ var (
 	lastReviewedNameIndex = 0
 )
 
+func initViewsMap(g *gocui.Gui) {
+	maxX, maxY := g.Size()
+	views[ViewNames] = &Window{-1, 3, 35, maxY - 1}
+	views[ViewText] = &Window{35, 3, maxX, maxY - 1}
+	views[ViewHelp] = &Window{-1, maxY - 2, maxX, maxY}
+}
+
 func InitGUI(inputData []byte, inputDataPath string) {
 	var err error
 
-	text, names, err = prepareData(inputData, inputDataPath)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	if names.Data.Meta.TotalNames == 0 {
-		fmt.Printf("\nNo names had been found in the document\n\n")
-		os.Exit(0)
-	}
+	textData = inputData
+	textDataPath = inputDataPath
 
 	g, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
@@ -50,6 +74,16 @@ func InitGUI(inputData []byte, inputDataPath string) {
 	}
 
 	g.Cursor = true
+
+	initViewsMap(g)
+	text, names, err = prepareData(textData, textDataPath, views[ViewText].width() - 1)
+	if err != nil {
+		log.Panic(err)
+	}
+	if names.Data.Meta.TotalNames == 0 {
+		fmt.Printf("\nNo names had been found in the document\n\n")
+		os.Exit(0)
+	}
 
 	g.SetManagerFunc(Layout)
 
@@ -108,8 +142,13 @@ func Keybindings(g *gocui.Gui) error {
 
 func Layout(g *gocui.Gui) error {
 	var err error
+	initViewsMap(g)
 
 	if err = viewStats(g); err != nil {
+		return err
+	}
+
+	if err = viewNames(g); err != nil {
 		return err
 	}
 
@@ -128,8 +167,8 @@ func Layout(g *gocui.Gui) error {
 }
 
 func viewNames(g *gocui.Gui) error {
-	_, maxY := g.Size()
-	if v, err := g.SetView("names", -1, 3, 35, maxY-1); err != nil {
+	vn := views[ViewNames]
+	if v, err := g.SetView("names", vn.x0, vn.y0, vn.x1, vn.y1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -140,8 +179,8 @@ func viewNames(g *gocui.Gui) error {
 }
 
 func viewText(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("text", 35, 3, maxX, maxY-1); err != nil {
+	vt := views[ViewText]
+	if v, err := g.SetView("text", vt.x0, vt.y0, vt.x1, vt.y1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
@@ -164,8 +203,8 @@ func viewStats(g *gocui.Gui) error {
 }
 
 func viewHelp(g *gocui.Gui) error {
-	maxX, maxY := g.Size()
-	if v, err := g.SetView("help", -1, maxY-2, maxX, maxY); err != nil {
+	vh := views[ViewHelp]
+	if v, err := g.SetView("help", vh.x0, vh.y0, vh.x1, vh.y1); err != nil {
 		if err != gocui.ErrUnknownView {
 			return err
 		}
